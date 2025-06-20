79fadacd67d9a5f35798507da0ff24f42847b95a
markgiovany <markgiovany11@gmail.com>
2025-06-20 13:26:21 -0500

    acceso
    Agregando el acceso de la API para obtener la información de los personajes

---
+++ main.js
@@ -0,0 +1,32 @@
+const marvel = {
+    render: () =>{
+    const urlAPI = 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=6a2421eec9a6fa554c7e5b9967620440&hash=600959899af81bf3ed3f45ae50cf5bdf';
+    const container = document.querySelector('#marvel-row');
+    let contentHTML = '';
+
+    
+    fetch(urlAPI)
+        .then(res=> res.json())
+        .then((json) =>{
+
+            for(const hero of json.data.results){
+
+                let urlHero = hero.urls[0].url;
+                contentHTML= `
+                <div class="col-md-4">
+                    <a href="${urlHero}" target="_blank">
+                        <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
+                    </a>
+                    <h3 class="title"> ${hero.name} </h3>
+                </div>
+                `
+                
+            }
+            container.innerHTML = contentHTML;
+        })
+
+ }
+    
+};
+
+marvel.render();
