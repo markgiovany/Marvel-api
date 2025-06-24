30212098c8d91139fd8fddb012587e686f74b5e7
luisMIGA <luismurad4825@gmail.com>
2025-06-23 20:23:01 -0500

    agregando imagen y nombre

--- main.js
+++ main.js
@@ -12,12 +12,13 @@ const marvel = {
             for(const hero of json.data.results){
 
                 let urlHero = hero.urls[0].url;
-                contentHTML= `
-                <div class="col-md-4">
-                    <a href="${urlHero}" target="_blank">
-                        <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
-                    </a>
-                    <h3 class="title"> ${hero.name} </h3>
+                contentHTML+= `
+                <div class="character">
+                                 
+                    <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
+                     <a href="${urlHero}" target="_blank">
+                        <h3 class="title"> ${hero.name} </h3>
+                    </a>    
                 </div>
                 `
                 
