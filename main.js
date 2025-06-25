7d70b1ec9a0ae34b2d76eb0ddb835b3b869d2d34
markgiovany <markgiovany11@gmail.com>
2025-06-24 23:23:42 -0500

    buscador
    Activando el buscador para que el usuario pueda buscar el personaje que quiere

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 const marvel = {
-  render: () => {
-    const urlAPI = 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=6a2421eec9a6fa554c7e5b9967620440&hash=600959899af81bf3ed3f45ae50cf5bdf';
+  render: (nameStartsWith = '') => {
+    
+    const urlAPI = nameStartsWith
+      ? `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${nameStartsWith}&ts=1&apikey=6a2421eec9a6fa554c7e5b9967620440&hash=600959899af81bf3ed3f45ae50cf5bdf`
+      : `https://gateway.marvel.com/v1/public/characters?ts=1&apikey=6a2421eec9a6fa554c7e5b9967620440&hash=600959899af81bf3ed3f45ae50cf5bdf`;
+
     const container = document.querySelector('#marvel-row');
     let contentHTML = '';
+
+    document.getElementById('searchBtn').addEventListener('click', () => {
+  const searchTerm = document.getElementById('searchInput').value.trim();
+  marvel.render(searchTerm);
+});
+
+
+document.getElementById('searchInput').addEventListener('keypress', () => {
+  if (p.key === 'Enter') {
+    document.getElementById('searchBtn').click();
+  }
+});
 
     fetch(urlAPI)
       .then(res => res.json())
       .then((json) => {
+        if (json.data.results.length === 0) {
+          container.innerHTML = '<p>No se encontraron personajes.</p>';
+          return;
+        }
+
         for (const hero of json.data.results) {
           const image = `${hero.thumbnail.path}.${hero.thumbnail.extension}`;
           const name = hero.name;
@@ -18,13 +39,13 @@ const marvel = {
           contentHTML += `
             <div class="character">
               <img src="${image}" alt="${name}">
-                <div class="character_content">
-                    <h3>${name}</h3>
-                    <p><strong>Cómics disponibles: </strong> ${comics}</p>
-                    <p>Series disponibles: ${series}</p>
-                    <p>Eventos disponibles: ${events}</p>
-                    <a href="${urlHero}" target="_blank">Ver más</a>
-                </div>
+              <div class="character_content">
+                <h3>${name}</h3>
+                <p><strong>Cómics disponibles: </strong> ${comics}</p>
+                <p>Series disponibles: ${series}</p>
+                <p>Eventos disponibles: ${events}</p>
+                <a href="${urlHero}" target="_blank">Ver más</a>
+              </div>
             </div>
           `;
         }
@@ -33,5 +54,9 @@ const marvel = {
       });
   }
 };
+
+
+
+
 
 marvel.render();
